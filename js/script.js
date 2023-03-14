027c7640d448b8d3aaa0a35f66fc21c1b16c3d59
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-03-14 14:12:14 +0200

    methods .fadeOut() та fadeIn()

--- js/script.js
+++ js/script.js
@@ -24,13 +24,32 @@
 
 // =============================================
 // Методи width, height:
-function widthAndHeight (element){
-    let className = '.'+element;
-    let w = $(className).width();
-    let h = $(className).height();
-    $('.mainText p').text('Ширина: ' + w + '; ' + 'Висота: ' + h + ';')
+// function widthAndHeight (element){
+//     let className = '.'+element;
+//     let w = $(className).width();
+//     let h = $(className).height();
+//     $('.mainText p').text('Ширина: ' + w + '; ' + 'Висота: ' + h + ';')
+// }
+// widthAndHeight('arrowDown');
+
+// =============================================
+// Метод .html():
+// $('.mainText').html('<b>New text</b><h2>title</h2>');
+
+// =============================================
+// Методи .fadeOut() та fadeIn():
+// $('.mainText').fadeOut(2000).fadeIn(3000);
+
+function fadeOutElement(element, time){
+    if(time > 5000 || time < 1000 ||isNaN(time)){
+        return false;
+    } else{
+        let className = '.' + element;
+        $(className).fadeOut(time);
+    }
 }
-widthAndHeight('arrowDown');
+fadeOutElement('mainText', 200)
+
 
 
 
